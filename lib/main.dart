a6f9b33e3ef3e684f835147baa7a443bc7600eb6
Mustafa Efe Güzel <mustafaefeguzel@pm.me>
2022-03-11 18:40:27 +0300

    Change accent color to cyan and add TODO.txt

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: ThemeData(
-          primarySwatch: Colors.deepPurple
+          primarySwatch: Colors.cyan
         ),
       home: ArticleList(Articles.FetchAll())
     );

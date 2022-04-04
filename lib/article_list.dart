7e696d0ef1e3b961ab475932bb022424bd7afdcf
Mustafa Efe Güzel <mustafaefeguzel@protonmail.com>
2022-04-04 22:30:47 +0300

    Translate articles and center images

--- lib/article_list.dart
+++ lib/article_list.dart
@@ -5,18 +5,20 @@ import 'package:read_math/models/article_model.dart';
 import 'package:read_math/navbar.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 
-class ArticleList extends StatelessWidget{
+class ArticleList extends StatelessWidget {
   final List<ArticleModel> articles;
+
   const ArticleList(this.articles);
 
-  Widget _itemThumbnail(ArticleModel thumbnailLocation){
+  Widget _itemThumbnail(ArticleModel thumbnailLocation) {
     return Container(
-        constraints: const BoxConstraints.tightFor(width: 100),
+        constraints: const BoxConstraints.tightFor(width: 110),
         child: ClipRRect(
             borderRadius: BorderRadius.circular(8.0),
             child: CachedNetworkImage(
-                imageUrl:thumbnailLocation.url,
-                placeholder: (context, url) => const Center(child: SizedBox(
+                imageUrl: thumbnailLocation.url,
+                placeholder: (context, url) =>
+                const Center(child: SizedBox(
                     width: 40,
                     height: 40,
                     child: CircularProgressIndicator()
@@ -27,37 +29,45 @@ class ArticleList extends StatelessWidget{
         ));
   }
 
-  Widget _itemTitle(ArticleModel titleLocation){
+  Widget _itemTitle(ArticleModel titleLocation) {
     return Text(titleLocation.heading, style: Styles.textDefault);
   }
 
-  Widget _locationTile(BuildContext context, int index){
+  Widget _locationTile(BuildContext context, int index) {
     final art = articles[index];
-    return ListTile(
-        contentPadding: const EdgeInsets.fromLTRB(5, 10, 5, 10),
-        leading: _itemThumbnail(art),
-        title: _itemTitle(art),
-        onTap: () {
-          Navigator.push(context,
-              MaterialPageRoute(builder: (context) => ArticleDetail(art)
-          ));
-        }
-    );
+    return Padding(
+        padding: const EdgeInsets.fromLTRB(5, 10, 5, 15),
+        child: InkWell(
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => ArticleDetail(art)
+                ));
+          },
+          child: Row(
+              children: <Widget>[
+                Container(
+                  padding: const EdgeInsets.fromLTRB(5, 0, 15, 0),
+                  child: _itemThumbnail(art),
+                ),
+                Flexible(
+                  child: _itemTitle(art),
+                )
+              ]
+          ),
+        ));
   }
-
-  @override
-  Widget build(BuildContext context){
-    return(Scaffold(
-        appBar: AppBar(
-          title: const Text("Articles", style: Styles.appbarStyle)
-        ),
-        drawer: const NavBar(),
-        body: ListView.builder(
-            itemCount: articles.length,
-            itemBuilder: _locationTile
-        )
+    @override
+    Widget build(BuildContext context) {
+      return (Scaffold(
+          appBar: AppBar(
+              title: const Text("Makaleler", style: Styles.appbarStyle)
+          ),
+          drawer: const NavBar(),
+          body: ListView.builder(
+              itemCount: articles.length,
+              itemBuilder: _locationTile
+          )
       )
-    );
+      );
+    }
   }
-
-}
